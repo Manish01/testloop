0d4a5d76eeb871432e4a5b0e99869fba855e08ee
Manish01 <manicche.maxgood@gmail.com>
2021-05-30 19:20:28 +0530

    Updated C file

--- Loop.c
+++ Loop.c
@@ -6,7 +6,7 @@ int main () {
 	
    /* for loop execution */
    for( a = 10; a < 20; a = a + 1 ){
-      printf("value of a: %d\n", a);
+      printf("hello Manish: %d\n", a);
    }
  
    return 0;
